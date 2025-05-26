3f7e2c5bfeac7bd7a892f437bd174bfc0177c232
Reuploads <sigmadarling3@gmail.com>
2025-05-26 16:50:25 -0500

    Update lua unicode escape.py

--- lua unicode escape.py
+++ lua unicode escape.py
@@ -10,8 +10,7 @@ def decode_lua_string(s):
         val = int(match.group(1))
         return chr(val) if val < 256 else match.group(0)
 
-    s = re.sub(r'\\d{1,3})', decode_octal, s)
-
+s = re.sub(r'\\\d{1,3}', decode_octal, s)
     # Decode hex escape sequences: \xHH
     s = re.sub(r'\\x([0-9A-Fa-f]{2})', lambda m: chr(int(m.group(1), 16)), s)
 
@@ -29,7 +28,6 @@ def decode_lua_string(s):
 
 # Regex to find Lua string literals ("..." and '...')
 string_pattern = re.compile(r'(["\'])(.*?)(?<!\\)\1', re.DOTALL)
-
 with open(input_file, "r", encoding="utf-8", errors="ignore") as f:
     content = f.read()
 
